e7e427b9d8602e32ba2448e6f79ac8bfa69f977e
Yadwinder Singh <ryker1990@gmail.com>
2016-07-15 00:01:19 +0530

    Update ADS1100.ino

--- Arduino/ADS1100.ino
+++ Arduino/ADS1100.ino
@@ -48,7 +48,7 @@ void loop()
   }
 
   // Output data to serial monitor
-  Serial.print("Digital value of analog input : ");
+  Serial.print("Digital Value of Analog Input : ");
   Serial.println(raw_adc);
   delay(500);
 }
